9de6d7398e659e1dce1843556b25835406a77133
Edward Hui <Edward@EdwarddeMacBook-Pro.local>
2018-12-17 21:51:39 +0800

    add new csv

--- chapter_16/highs_lows.py
+++ chapter_16/highs_lows.py
@@ -4,36 +4,37 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 
 # Get dates, high, and low temperatures from file.
-filename = 'death_valley_2014.csv'
+#filename = 'sitka_weather_2014.csv'
+filename='death_valley_2014.csv'
 with open(filename) as f:
-    reader = csv.reader(f)
-    header_row = next(reader)
-
-    dates, highs, lows = [], [], []
-    for row in reader:
-        try:
-            current_date = datetime.strptime(row[0], "%Y-%m-%d")
-            high = int(row[1])
-            low = int(row[3])
-        except ValueError:
-            print(current_date, 'missing data')
-        else:
-            dates.append(current_date)
-            highs.append(high)
-            lows.append(low)
-
-# Plot data.
-fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates, highs, c='red', alpha=0.5)
-plt.plot(dates, lows, c='blue', alpha=0.5)
-plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
-
-# Format plot.
-title = "Daily high and low temperatures - 2014\nDeath Valley, CA"
-plt.title(title, fontsize=20)
-plt.xlabel('', fontsize=16)
+	reader = csv.reader(f)
+	header_row = next(reader)
+	# for index,column_header in enumerate(header_row):
+	# 	print(index,column_header)
+	dates,highs,lows=[],[],[]
+	for row in reader:
+		try:
+			current_date=datetime.strptime(row[0],"%Y-%m-%d")
+			high=int(row[1])
+			low=int(row[3])
+		except ValueError:
+			print(current_date,'missing data')
+		else:
+			dates.append(current_date)
+			highs.append(int(row[1]))
+			lows.append(int(row[3]))
+	#print(highs)
+#根据数据绘制图形
+fig=plt.figure(dpi=100,figsize=(10,6))
+plt.plot(dates,highs,c='red',alpha=0.5)
+plt.plot(dates,lows,c='blue',alpha=0.5)
+plt.fill_between(dates,highs,lows,facecolor='blue',alpha=0.1)
+#设置图形的格式
+plt.title('Daily high temperature, - 2014',fontsize=24)
+plt.xlabel('',fontsize=16)
 fig.autofmt_xdate()
-plt.ylabel("Temperature (F)", fontsize=16)
-plt.tick_params(axis='both', which='major', labelsize=16)
-
+plt.ylabel('Temperature (F)',fontsize=16)
+plt.tick_params(axis='both',which='major',labelsize=16)
 plt.show()
+
+
